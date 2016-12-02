46b488fb6b2a8d6b174b823c14bf6b79c85aea63
Eric Chow <echow@Eric-Chows-Mac-mini.local>
2016-12-02 16:35:03 -0500

    finally understand what's going on. steps 1-4 coded

--- run_analysis.R
+++ run_analysis.R
@@ -21,7 +21,7 @@ features  <- fread("UCI HAR Dataset/features.txt", header=FALSE, strip.white=TRU
 # read test data --------------------------------------
 subject_test <- fread("UCI HAR Dataset/test/subject_test.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
 x_test <- fread("UCI HAR Dataset/test/x_test.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
-y_test <- fread("UCI HAR Dataset/test/y_test.txt", header=FALSE, strip.white=TRUE)
+y_test <- fread("UCI HAR Dataset/test/y_test.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
 
 inertial_signals_test <- NULL
 test_inert <- list.files("UCI HAR Dataset/test/Inertial Signals")
@@ -51,66 +51,104 @@ names(activity_labels) <- c("activityId", "activityDesc")
 head(activity_labels) # there are 6 activities
 nrow(activity_labels)
 
-# THERE ARE LOTS OF FEATURES
+# THERE ARE 561 FEATURES
 names(features) <- c("featureId", "featureDesc")
 head(features) # there are 561 features
 nrow(features)
+features$featureLabel <- gsub("[-()]", "", features$featureDesc) # make a safe var name
 
-# Each window was 128 readings, a vector of features obtained
 
+# these are the features specifying means or standard deviations
+f_mean <- grep("mean()", features$featureDesc, fixed=TRUE)
+f_std <- grep("std()", features$featureDesc, fixed=TRUE)
+meanstd <- sort(c(f_mean, f_std))
+
+features$featureDesc[meanstd]
+
+
+# TEST ---------------------------------------------------------
 names(subject_test) <- c("subjectId")
 head(subject_test)
-nrow(subject_test) # 2947 window samples
+nrow(subject_test)  # 2947 window samples
 table(subject_test)
 
-
+# the test set
 head(x_test)
-nrow(x_test) # 2947
-ncol(x_test) # 561
+nrow(x_test) # 2947 window samples
+ncol(x_test) # 561 features
 
-length(inertial_signals_train) # 9 iniertial signals
-
-head(inertial_signals_train[[1]])
-nrow(inertial_signals_train[[1]])   # 7352
-ncol(inertial_signals_train[[1]])   # 128 cols
-
-head(inertial_signals_train[[2]])
-nrow(inertial_signals_train[[2]])   # 11337 rows ?!?!?!
-ncol(inertial_signals_train[[2]])   # 196 cols ?!?!?!
-
-# ....
-
+# the test labels? - 1 of 6 activities
+names(y_test) <- c("activityId")
 head(y_test)
-nrow(y_test) # 2947
-ncol(y_test) # 1 col
+nrow(y_test) # 2947 window samples
+ncol(y_test) # 1
+table(y_test)
+
+# TRAIN ---------------------------------------------------------
+names(subject_train) <- c("subjectId")
+head(subject_train)
+nrow(subject_train)  # 2947 window samples
+table(subject_train)
+
+# the train set
+head(x_train)
+nrow(x_train) # 7352 window samples
+ncol(x_train) # 561 features
+
+# the train labels? - 1 of 6 activities
+names(y_train) <- c("activityId")
+head(y_train)
+nrow(y_train) # 7352 window samples
+ncol(y_train) # 1
+table(y_train)
+
+#
+# # Each window was 128 readings, a vector of features obtained
+#
+# length(inertial_signals_test) # 9 iniertial signals
+#
+# head(inertial_signals_test[[1]])
+# nrow(inertial_signals_test[[1]])   # 2947 samples
+# ncol(inertial_signals_test[[1]])   # 128 cols
+# # ....
+# head(inertial_signals_test[[9]])
+# nrow(inertial_signals_test[[9]])   # 2947
+# ncol(inertial_signals_test[[9]])   # 128 cols
+#
+# Tidy dataset: avg of each variable, activity (6), subject (30)
+
+
+# -------------------------------------------------------------
+# 1. Merge the training and test datasets
+# -------------------------------------------------------------
+subject <- rbind(subject_test, subject_train)
+x <- rbind(x_test, x_train)
+y <- rbind(y_test, y_train)
+
+data <- cbind(subject, y, x)
+
+# -------------------------------------------------------------
+# 2. Extract only measurements on mean and std
+# -------------------------------------------------------------
+meanstd2 <- meanstd + 2 # shift right by 2 to skip subject and activity ID
+data_ <- data[,c(1:2, meanstd2)]  # select only rows with mean() or std() in vars
+head(data_)
+
+# -------------------------------------------------------------
+# 3. Uses descriptive activity names
+# -------------------------------------------------------------
+data_$activity <- factor(data_$activity, label=activity_labels$activityDesc)
+
+# -------------------------------------------------------------
+# 4. label the dataset with variable names
+# -------------------------------------------------------------
+names(data_) <- c("subjectId", "activityId", features[meanstd,"featureLabel"], "activity")
+data_ <- data_[,c(1,2,ncol(data_),3:(ncol(data_)-1))] # reorder cols
+head(data_)
+
+data_$tBodyAcc-mean()-X
+
+
+
 
 #        ~ fin ~
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
