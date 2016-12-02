d0f6b1105908bb9e4a944b3cc6e0beb2c5aae6cb
Eric Chow <echow@Eric-Chows-Mac-mini.local>
2016-12-02 17:42:56 -0500

    step 5 coded, need to update README.md and CODEBOOK.md

--- run_analysis.R
+++ run_analysis.R
@@ -23,40 +23,19 @@ subject_test <- fread("UCI HAR Dataset/test/subject_test.txt", header=FALSE, str
 x_test <- fread("UCI HAR Dataset/test/x_test.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
 y_test <- fread("UCI HAR Dataset/test/y_test.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
 
-inertial_signals_test <- NULL
-test_inert <- list.files("UCI HAR Dataset/test/Inertial Signals")
-for (file in test_inert) {
-	message("reading in inertial signal: ", file, "...")
-	inertial_signals_test[[file]] <- fread(sprintf("UCI HAR Dataset/test/Inertial Signals/%s",file), header=FALSE, strip.white=TRUE, data.table=FALSE)
-}
-names(inertial_signals_test)
-
 # read train data --------------------------------------
 subject_train <- fread("UCI HAR Dataset/train/subject_train.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
 x_train <- fread("UCI HAR Dataset/train/x_train.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
 y_train <- fread("UCI HAR Dataset/train/y_train.txt", header=FALSE, strip.white=TRUE, data.table=FALSE)
 
-inertial_signals_train <- NULL
-train_inert <- list.files("UCI HAR Dataset/train/Inertial Signals")
-for (file in train_inert) {
-	message("reading in inertial signal: ", file, "...")
-	inertial_signals_train[[file]] <- fread(sprintf("UCI HAR Dataset/train/Inertial Signals/%s",file), header=FALSE, strip.white=TRUE, data.table=FALSE)
-}
-names(inertial_signals_train)
-
 # look at data -----------------------------------------------
 
 # THERE ARE 6 ACTIVITIES
 names(activity_labels) <- c("activityId", "activityDesc")
-head(activity_labels) # there are 6 activities
-nrow(activity_labels)
 
 # THERE ARE 561 FEATURES
 names(features) <- c("featureId", "featureDesc")
-head(features) # there are 561 features
-nrow(features)
 features$featureLabel <- gsub("[-()]", "", features$featureDesc) # make a safe var name
-
 
 # these are the features specifying means or standard deviations
 f_mean <- grep("mean()", features$featureDesc, fixed=TRUE)
@@ -68,55 +47,13 @@ features$featureDesc[meanstd]
 
 # TEST ---------------------------------------------------------
 names(subject_test) <- c("subjectId")
-head(subject_test)
-nrow(subject_test)  # 2947 window samples
-table(subject_test)
-
-# the test set
-head(x_test)
-nrow(x_test) # 2947 window samples
-ncol(x_test) # 561 features
-
 # the test labels? - 1 of 6 activities
 names(y_test) <- c("activityId")
-head(y_test)
-nrow(y_test) # 2947 window samples
-ncol(y_test) # 1
-table(y_test)
 
 # TRAIN ---------------------------------------------------------
 names(subject_train) <- c("subjectId")
-head(subject_train)
-nrow(subject_train)  # 2947 window samples
-table(subject_train)
-
-# the train set
-head(x_train)
-nrow(x_train) # 7352 window samples
-ncol(x_train) # 561 features
-
 # the train labels? - 1 of 6 activities
 names(y_train) <- c("activityId")
-head(y_train)
-nrow(y_train) # 7352 window samples
-ncol(y_train) # 1
-table(y_train)
-
-#
-# # Each window was 128 readings, a vector of features obtained
-#
-# length(inertial_signals_test) # 9 iniertial signals
-#
-# head(inertial_signals_test[[1]])
-# nrow(inertial_signals_test[[1]])   # 2947 samples
-# ncol(inertial_signals_test[[1]])   # 128 cols
-# # ....
-# head(inertial_signals_test[[9]])
-# nrow(inertial_signals_test[[9]])   # 2947
-# ncol(inertial_signals_test[[9]])   # 128 cols
-#
-# Tidy dataset: avg of each variable, activity (6), subject (30)
-
 
 # -------------------------------------------------------------
 # 1. Merge the training and test datasets
@@ -146,9 +83,30 @@ names(data_) <- c("subjectId", "activityId", features[meanstd,"featureLabel"], "
 data_ <- data_[,c(1,2,ncol(data_),3:(ncol(data_)-1))] # reorder cols
 head(data_)
 
-data_$tBodyAcc-mean()-X
+# -------------------------------------------------------------
+# 5. create independent tidy dataset with average of variable
+# for each activity (walk, etc n=6), and each subject (n=1..30)
+# -------------------------------------------------------------
+table(data_$subjectId, data_$activity)
+tidy <- expand.grid(subjectId=sort(unique(data_$subjectId)), activity=sort(unique(data_$activity)))
+summary(tidy)
+head(tidy)
 
+#  for a given subject-activity
+meansarray <- NULL
+for (i in 1:nrow(tidy)) { # calculate the col means
+	b <- data_[(data_$subjectId==tidy[i,"subjectId"]) & (data_$activity==tidy[i,"activity"]), -c(1:3) ]
+	means <- apply(b,2,FUN=mean)
+	meansarray <- rbind(meansarray, means)
+}
+# if it all worked out, bind the subject and means together
+if (nrow(meansarray) == nrow(tidy)) {
+		row.names(meansarray) <- 1:nrow(meansarray)
+		tidydata = cbind(tidy, meansarray)
+}
 
+# write it to file
+write.table(tidydata, file="tidydata.txt", sep=", ", col.names=TRUE, row.names=FALSE)
 
 
 #        ~ fin ~
